b052bbd76faecb1526fdeab34d2e4d4e06fe7d60
Alexander Eckhart | CHN <alexander.eckhart@chateaunoir.ro>
2017-04-21 23:21:40 +0300

    improvement: Refactor component scripts

--- data/scripts/component.js
+++ data/scripts/component.js
@@ -22,7 +22,7 @@
     // Sidebar Options
 	Sidebar.prototype.options = {
         trigger: '.toggle-sidebar',             // element that toggles the sidebar
-        target:  '.sidebar',                    // wrapper that holds the sidebar  
+        target:  '#sidebar',                    // wrapper that holds the sidebar  
         sidebar: {
             visibleClass: 'sidebar-visible',            //
             item: '.sidebar-item-has-children',         //
@@ -37,22 +37,17 @@
         var _self = this,
             elements,
             triggerElem = document.querySelectorAll(this.options.trigger),
-            targetElem = document.querySelector(this.options.target),
-            sidebarElem = document.querySelector(_self.options.target);
+            targetElem = document.querySelector(this.options.target);
 
-        if (targetElem && triggerElem) {
-            triggerElem.forEach(function(trig){
-                trig.addEventListener('click', function(){
-                    if(targetElem.classList.contains(_self.options.sidebar.visibleClass)){
-                        _self.hide();
-                    } else {
-                        _self.show();
-                    }
-                });
+        if (targetElem) {
+            if (targetElem.classList.contains(_self.options.sidebar.visibleClass)) {
+                _self.isVisible = true;
+            }
+
+            XXX.handleEvent(document, 'click', _self.options.trigger, function(){
+                _self.isVisible ? _self.hide() : _self.show();
             });
-        }
 
-        if (sidebarElem) {
             var closeAllSidebarItems = function(wrapper, element, classToRemove) {
                 elements = wrapper.querySelectorAll('.'+element);
                 elements.forEach(function(element) {
@@ -61,7 +56,6 @@
             }
 
             var toggleSidebarItems = function(wrapper, element, classToToggle){
-                
                 if(element.parentNode.classList.contains(classToToggle)) {
                     element.parentNode.classList.remove(classToToggle);
                 } else {
@@ -70,14 +64,16 @@
                 }
             }
 
-            XXX.handleEvent(sidebarElem, 'click', _self.options.sidebar.itemBtn, function(){
-                toggleSidebarItems(sidebarElem, this, _self.options.sidebar.itemActiveClass);
+            XXX.handleEvent(targetElem, 'click', _self.options.sidebar.itemBtn, function(){
+                toggleSidebarItems(targetElem, this, _self.options.sidebar.itemActiveClass);
             });
         }
         
     }
 
     Sidebar.prototype.show = function() {
+        this.isVisible = true;
+
         var targetElem = document.querySelector(this.options.target);
         targetElem.classList.add(this.options.sidebar.visibleClass);
 
@@ -87,6 +83,8 @@
     }
 
     Sidebar.prototype.hide = function() {
+        this.isVisible = false;
+
         var targetElem = document.querySelector(this.options.target);
         targetElem.classList.remove(this.options.sidebar.visibleClass);
 
--- dist/js/sidebar-menu.js
+++ dist/js/sidebar-menu.js
@@ -55,7 +55,7 @@ var XXX = {
     // Sidebar Options
 	Sidebar.prototype.options = {
         trigger: '.toggle-sidebar',             // element that toggles the sidebar
-        target:  '.sidebar',                    // wrapper that holds the sidebar  
+        target:  '#sidebar',                    // wrapper that holds the sidebar  
         sidebar: {
             visibleClass: 'sidebar-visible',            //
             item: '.sidebar-item-has-children',         //
@@ -70,22 +70,17 @@ var XXX = {
         var _self = this,
             elements,
             triggerElem = document.querySelectorAll(this.options.trigger),
-            targetElem = document.querySelector(this.options.target),
-            sidebarElem = document.querySelector(_self.options.target);
+            targetElem = document.querySelector(this.options.target);
 
-        if (targetElem && triggerElem) {
-            triggerElem.forEach(function(trig){
-                trig.addEventListener('click', function(){
-                    if(targetElem.classList.contains(_self.options.sidebar.visibleClass)){
-                        _self.hide();
-                    } else {
-                        _self.show();
-                    }
-                });
+        if (targetElem) {
+            if (targetElem.classList.contains(_self.options.sidebar.visibleClass)) {
+                _self.isVisible = true;
+            }
+
+            XXX.handleEvent(document, 'click', _self.options.trigger, function(){
+                _self.isVisible ? _self.hide() : _self.show();
             });
-        }
 
-        if (sidebarElem) {
             var closeAllSidebarItems = function(wrapper, element, classToRemove) {
                 elements = wrapper.querySelectorAll('.'+element);
                 elements.forEach(function(element) {
@@ -94,7 +89,6 @@ var XXX = {
             }
 
             var toggleSidebarItems = function(wrapper, element, classToToggle){
-                
                 if(element.parentNode.classList.contains(classToToggle)) {
                     element.parentNode.classList.remove(classToToggle);
                 } else {
@@ -103,14 +97,16 @@ var XXX = {
                 }
             }
 
-            XXX.handleEvent(sidebarElem, 'click', _self.options.sidebar.itemBtn, function(){
-                toggleSidebarItems(sidebarElem, this, _self.options.sidebar.itemActiveClass);
+            XXX.handleEvent(targetElem, 'click', _self.options.sidebar.itemBtn, function(){
+                toggleSidebarItems(targetElem, this, _self.options.sidebar.itemActiveClass);
             });
         }
         
     }
 
     Sidebar.prototype.show = function() {
+        this.isVisible = true;
+
         var targetElem = document.querySelector(this.options.target);
         targetElem.classList.add(this.options.sidebar.visibleClass);
 
@@ -120,6 +116,8 @@ var XXX = {
     }
 
     Sidebar.prototype.hide = function() {
+        this.isVisible = false;
+
         var targetElem = document.querySelector(this.options.target);
         targetElem.classList.remove(this.options.sidebar.visibleClass);
 
